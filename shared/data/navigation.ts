fa21cbccb0dabf80d910557cc1b11e59b64e9e79
_oli <oli@appsdevteam.com>
2026-09-18 17:24:30 +0200

    Update navigation.ts (#42)
    Co-authored-by: Honza Pobořil <honza@poboril.cz>

--- shared/data/navigation.ts
+++ shared/data/navigation.ts
@@ -79,6 +79,13 @@ export const navigationItems: NavigationMenuItem[] = [
         target: '_blank',
       },
       {
+        label: 'BTC Gains Checker',
+        description: 'Privátní sledování hodnoty svého BTC portfolia',
+        icon: 'lucide:chart-line',
+        to: 'https://btcgainschecker.com',
+        target: '_blank',
+      },
+      {
         label: 'Získej financování',
         description: 'Pomůžeme tu najít sponzory tvého projektu.',
         to: '/finance',
